4003d6405b1e671272da36debe6e6e8b92f8b061
Iris <iris.devillars@gmail.com>
2023-05-16 09:51:28 -0700

    feat: implement verify endpoints for starkfighter

---
+++ src/endpoints/quests/starkfighter/mod.rs
@@ -0,0 +1,4 @@
+pub mod models;
+pub mod verify_has_played;
+pub mod verify_has_score_greater_than_100;
+pub mod verify_has_score_greater_than_50;
---
+++ src/endpoints/quests/starkfighter/models.rs
@@ -0,0 +1,23 @@
+use serde::{Deserialize, Serialize};
+
+pub struct CompletedTasks {
+    address: String,
+    task_id: u32,
+}
+
+#[derive(Deserialize)]
+pub struct ScoreResponse {
+    pub owner: String,
+    pub score: f64,
+}
+
+#[derive(Deserialize)]
+pub struct StarkfighterQuery {
+    pub addr: String,
+}
+
+#[derive(Serialize)]
+pub struct QueryError {
+    pub error: String,
+    pub res: bool,
+}
