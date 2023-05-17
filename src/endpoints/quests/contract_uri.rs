fca1348be0c1cfa3f0246b5b604bbd3d422f3c92
Thomas Marchand <thomas.marchand@tuta.io>
2023-05-18 00:19:40 +0200

    feat: add contract_uri

---
+++ src/endpoints/quests/contract_uri.rs
@@ -0,0 +1,35 @@
+use crate::models::AppState;
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use serde::Serialize;
+use std::sync::Arc;
+
+#[derive(Serialize)]
+pub struct TokenURI {
+    name: String,
+    description: String,
+    image: String,
+    banner_image_url: String,
+    external_link: String,
+}
+
+pub async fn handler(State(state): State<Arc<AppState>>) -> Response {
+    let response = TokenURI {
+        name: "Starknet Quest".into(),
+        description: "Starknet Quest - The Collection of your Starknet achievements".into(),
+        image: format!(
+            "{}/visuals/starknetquest.webp",
+            state.conf.variables.app_link
+        ),
+        banner_image_url: format!(
+            "{}/visuals/starknetquestBanner.webp",
+            state.conf.variables.app_link
+        ),
+        external_link: format!("{}/", state.conf.variables.app_link),
+    };
+    (StatusCode::OK, Json(response)).into_response()
+}
