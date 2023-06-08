4d121571024a1515e9345c8fdb00384c8193b38e
Thomas Marchand <thomas.marchand@tuta.io>
2023-06-08 11:46:45 +0200

    feat: revert listening to msg

--- src/main.rs
+++ src/main.rs
@@ -125,7 +125,7 @@ async fn main() {
         .layer(cors);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], conf.server.port));
-    println!("server: listening on http://127.0.0.1:{}", conf.server.port);
+    println!("server: listening on http://0.0.0.0:{}", conf.server.port);
     axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
