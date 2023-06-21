8f8d46b490193790e3d1b37247862dce7d7e52f0
Iris <iris.devillars@gmail.com>
2023-06-21 12:11:02 +0200

    fix: twitter api endpoints redirect

--- src/endpoints/quests/avnu/verify_twitter_fw.rs
+++ src/endpoints/quests/avnu/verify_twitter_fw.rs
@@ -2,28 +2,23 @@ use std::sync::Arc;
 
 use crate::{
     models::{AppState, VerifyQuery},
-    utils::{get_error_redirect, success_redirect, CompletedTasksTrait},
+    utils::{get_error, CompletedTasksTrait},
 };
 use axum::{
     extract::{Query, State},
+    http::StatusCode,
     response::IntoResponse,
+    Json,
 };
+use serde_json::json;
 
 pub async fn handler(
     State(state): State<Arc<AppState>>,
     Query(query): Query<VerifyQuery>,
 ) -> impl IntoResponse {
-    let quest_id = 3;
     let task_id = 14;
-    let error_redirect_uri = format!(
-        "{}/quest/{}?task_id={}&res=false",
-        state.conf.variables.app_link, quest_id, task_id
-    );
     match state.upsert_completed_task(query.addr, task_id).await {
-        Ok(_) => success_redirect(format!(
-            "{}/quest/{}?task_id={}&res=true",
-            state.conf.variables.app_link, quest_id, task_id
-        )),
-        Err(e) => get_error_redirect(error_redirect_uri, format!("{}", e)),
+        Ok(_) => (StatusCode::OK, Json(json!({"res": true}))).into_response(),
+        Err(e) => get_error(format!("{}", e)),
     }
 }
