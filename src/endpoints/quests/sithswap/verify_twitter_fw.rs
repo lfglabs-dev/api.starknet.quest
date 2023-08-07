24b46384876ed4a268d5fb545b1591f8e7b59b78
Iris <iris.devillars@gmail.com>
2023-08-07 09:51:50 +0200

    fix: add check addr 0 in verify twitter tasks

--- src/endpoints/quests/sithswap/verify_twitter_fw.rs
+++ src/endpoints/quests/sithswap/verify_twitter_fw.rs
@@ -11,12 +11,16 @@ use axum::{
     Json,
 };
 use serde_json::json;
+use starknet::core::types::FieldElement;
 
 pub async fn handler(
     State(state): State<Arc<AppState>>,
     Query(query): Query<VerifyQuery>,
 ) -> impl IntoResponse {
     let task_id = 21;
+    if query.addr == FieldElement::ZERO {
+        return get_error("Please connect your wallet first".to_string());
+    }
     match state.upsert_completed_task(query.addr, task_id).await {
         Ok(_) => (StatusCode::OK, Json(json!({"res": true}))).into_response(),
         Err(e) => get_error(format!("{}", e)),
