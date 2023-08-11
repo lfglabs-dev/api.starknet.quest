73eb56c4cf24c6bd75acc5dd91b01f99850f8c24
Iris <iris.devillars@gmail.com>
2023-08-11 16:01:22 +0200

    fix: update get_quest to take into account expiry

--- src/endpoints/get_quest.rs
+++ src/endpoints/get_quest.rs
@@ -7,6 +7,8 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Json},
 };
+use chrono::Utc;
+use mongodb::bson;
 use mongodb::bson::doc;
 use serde::Deserialize;
 use std::sync::Arc;
@@ -21,11 +23,39 @@ pub async fn handler(
     Query(query): Query<GetQuestsQuery>,
 ) -> impl IntoResponse {
     let collection = state.db.collection::<QuestDocument>("quests");
-    match collection
-        .find_one(doc! {"id": query.id, "disabled" : false}, None)
-        .await
-    {
-        Ok(Some(quest)) => (StatusCode::OK, Json(quest)).into_response(),
+    let current_timestamp = bson::DateTime::from_millis(Utc::now().timestamp_millis());
+    let filter = doc! {
+        "$and": [
+            {
+                "id": query.id
+            },
+            {
+                "$or": [
+                    {
+                        "expiry": {
+                            "$exists": false
+                        }
+                    },
+                    {
+                        "expiry": {
+                            "$gt": current_timestamp
+                        }
+                    }
+                ]
+            },
+            {
+                "disabled": false
+            }
+        ]
+    };
+    match collection.find_one(filter, None).await {
+        Ok(Some(mut quest)) => {
+            if let Some(expiry) = &quest.expiry {
+                let timestamp = expiry.timestamp_millis().to_string();
+                quest.expiry_timestamp = Some(timestamp);
+            }
+            (StatusCode::OK, Json(quest)).into_response()
+        }
         Ok(None) => get_error("Quest not found".to_string()),
         Err(_) => get_error("Error querying quest".to_string()),
     }
