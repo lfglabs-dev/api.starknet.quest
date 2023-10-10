ee32b908c7f5140cc9c4e7ea2031dcf06d9ef790
Iris <iris.devillars@gmail.com>
2023-10-10 12:15:25 +0200

    fix: use lazy static

--- src/endpoints/achievements/verify_whitelisted.rs
+++ src/endpoints/achievements/verify_whitelisted.rs
@@ -1,18 +1,20 @@
 use crate::models::Nft;
 use regex::Regex;
 
-pub fn is_braavos_whitelisted(nft: &Nft, unique_nfts: &mut Vec<String>) {
-    let whitelist_patterns = vec![
-        r"Starknet Onboarding Journey( NFT)?",
-        r"Starknet Identity Journey",
-        r"Starknet Exchange Journey",
-        r"Starknet Mobile Journey",
-        r"(Starknet Journey Coin NFT|starknet-journey-coin)",
+lazy_static::lazy_static! {
+    static ref BRAAVOS_WHITELIST : Vec<Regex> = vec![
+        Regex::new(r"Starknet Onboarding Journey( NFT)?").unwrap(),
+        Regex::new(r"Starknet Identity Journey").unwrap(),
+        Regex::new(r"Starknet Exchange Journey").unwrap(),
+        Regex::new(r"Starknet Mobile Journey").unwrap(),
+        Regex::new(r"(Starknet Journey Coin NFT|starknet-journey-coin)").unwrap()
     ];
+}
+
+pub fn is_braavos_whitelisted(nft: &Nft, unique_nfts: &mut Vec<String>) {
     if let Some(name) = nft.name.as_ref() {
-        for pattern in &whitelist_patterns {
-            let re = Regex::new(pattern).unwrap();
-            if re.is_match(name) && !unique_nfts.contains(name) {
+        for pattern in &*BRAAVOS_WHITELIST {
+            if pattern.is_match(name) && !unique_nfts.contains(name) {
                 unique_nfts.push(name.clone());
                 return;
             }
