a39d4ed18c3bb30c6937306e06762364046ba1e2
ayushtom <tomar2585@gmail.com>
2023-10-31 05:02:42 +0530

    dev: revert unnec code

--- src/endpoints/leaderboard/get_ranking.rs
+++ src/endpoints/leaderboard/get_ranking.rs
@@ -1,23 +1,3 @@
-use crate::{models::AppState, utils::get_error};
-use axum::{
-    extract::{Query, State},
-    response::IntoResponse,
-    Json,
-};
-
-use futures::TryStreamExt;
-use mongodb::bson::{doc, Document};
-use reqwest::StatusCode;
-use serde::{Deserialize, Serialize};
-use starknet::core::types::FieldElement;
-use std::sync::Arc;
-
-#[derive(Debug, Serialize, Deserialize)]
-
-pub struct GetCompletedQuestsQuery {
-    addr: FieldElement,
-}
-
 
 /*
  this endpoint will return leaderboard ranking for one address
@@ -31,31 +11,3 @@ pub struct GetCompletedQuestsQuery {
 5) "last index" can also act as a flag value to check if there is a previous page or next page
 (last_index === -1 then no previous page && last_index === total.documents.length then no next page)
  */
-
-pub async fn handler(
-    State(state): State<Arc<AppState>>,
-    Query(query): Query<GetCompletedQuestsQuery>,
-) -> impl IntoResponse {
-    let address = query.addr.to_string();
-    println!("{}", address);
-    let pipeline = vec![
-        doc! {
-            "$match": doc! {
-                "address": address
-            }
-        },
-
-    ];
-    let tasks_collection = state.db.collection::<Document>("user_exp");
-    match tasks_collection.aggregate(pipeline, None).await {
-        Ok(mut cursor) => {
-            let mut quests: Vec<u32> = Vec::new();
-            while let Some(result) = cursor.try_next().await.unwrap() {
-                quests.push(result.get("experience").unwrap().as_i32().unwrap() as u32);
-            }
-            print!("{:?}", quests);
-            (StatusCode::OK, Json(quests)).into_response()
-        }
-        Err(_) => get_error("Error querying quests".to_string()),
-    }
-}
