7546ae39165f71dcb4a760737507b7a498bd632a
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2023-10-31 15:58:31 +0100

    new expired (bool) field

--- src/endpoints/get_quests.rs
+++ src/endpoints/get_quests.rs
@@ -1,19 +1,10 @@
-use crate::{
-    models::{AppState, QuestDocument},
-    utils::get_error,
-};
-use axum::{
-    extract::State,
-    http::StatusCode,
-    response::{IntoResponse, Json},
-};
-use chrono::Utc;
-use futures::StreamExt;
-use futures::TryStreamExt;
-use mongodb::bson;
-use mongodb::bson::doc;
+use crate::models::QuestDocument;
+use crate::{models::AppState, utils::get_error};
+use axum::response::{IntoResponse, Json};
+use axum::{extract::State, http::StatusCode};
+use futures::stream::StreamExt;
+use mongodb::bson::{doc, from_document};
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use std::sync::Arc;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -23,61 +14,46 @@ pub struct NFTItem {
 }
 
 pub async fn handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    let collection = state.db.collection::<QuestDocument>("quests");
-    let current_timestamp = bson::DateTime::from_millis(Utc::now().timestamp_millis());
-    let filter = doc! {
-        "$and": [
-        {
-            "$or": [
-                {
-                    "expiry": {
-                        "$exists": false
-                    }
-                },
-                {
-                    "expiry": {
-                        "$gt": current_timestamp
-                    }
-                }
-            ]
+    let pipeline = vec![
+        doc! {
+            "$match": {
+                "disabled": false,
+                "hidden": false,
+            }
         },
-        {
-            "hidden": false
-        }
-    ]
-    };
-
-    match collection.find(Some(filter), None).await {
-        Ok(cursor) => {
-            let quests_temp: Vec<QuestDocument> = cursor
-                .map(|result| {
-                    result.map(|mut quest: QuestDocument| {
-                        if let Some(expiry) = &quest.expiry {
-                            let timestamp = expiry.timestamp_millis().to_string();
-                            quest.expiry_timestamp = Some(timestamp);
-                        }
-                        quest
-                    })
-                })
-                .try_collect()
-                .await
-                .unwrap_or_else(|_| vec![]);
-            let mut res: HashMap<String, Vec<QuestDocument>> = HashMap::new();
-            for quest in quests_temp {
-                let category = quest.category.clone();
-                if res.contains_key(&category) {
-                    let quests = res.get_mut(&category).unwrap();
-                    quests.push(quest);
-                } else {
-                    res.insert(category, vec![quest]);
+        doc! {
+            "$addFields": {
+                "expired": {
+                    "$cond": [
+                        {
+                            "$and": [
+                                { "$gte": ["$expiry", 0] },
+                                { "$lt": ["$expiry", "$$NOW"] },
+                            ]
+                        },
+                        true,
+                        false
+                    ]
                 }
             }
+        },
+    ];
+    let collection = state.db.collection::<QuestDocument>("quests");
 
-            if res.is_empty() {
-                get_error("No quests found".to_string())
-            } else {
-                (StatusCode::OK, Json(res)).into_response()
+    match collection.aggregate(pipeline, None).await {
+        Ok(mut cursor) => {
+            let mut quests: Vec<QuestDocument> = Vec::new();
+            while let Some(result) = cursor.next().await {
+                match result {
+                    Ok(document) => {
+                        if let Ok(quest) = from_document::<QuestDocument>(document) {
+                            quests.push(quest);
+                        }
+                    }
+                    _ => continue,
+                }
             }
+            (StatusCode::OK, Json(quests)).into_response()
         }
         Err(_) => get_error("Error querying quests".to_string()),
     }
