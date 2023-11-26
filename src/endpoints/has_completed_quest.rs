20aa51af7797dfb296ddcd73391c78227e63dd81
ayushtom <tomar2585@gmail.com>
2023-11-26 17:01:51 +0530

    feat: add endpoint to get quest completion status

---
+++ src/endpoints/has_completed_quest.rs
@@ -0,0 +1,105 @@
+use crate::{models::AppState, utils::get_error};
+use axum::{
+    extract::{Query, State},
+    response::IntoResponse,
+    Json,
+};
+
+use futures::TryStreamExt;
+use mongodb::bson::{doc, Document};
+use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+use starknet::core::types::FieldElement;
+use std::sync::Arc;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HasCompletedQuestsQuery {
+    addr: FieldElement,
+    quest_id: u32,
+}
+
+pub async fn handler(
+    State(state): State<Arc<AppState>>,
+    Query(query): Query<HasCompletedQuestsQuery>,
+) -> impl IntoResponse {
+    let address = query.addr.to_string();
+    let quest_id = query.quest_id;
+    let pipeline = vec![
+        doc! {
+        "$match": doc! {
+            "address": address,
+        }
+    },
+        doc! {
+        "$lookup": doc! {
+            "from": "tasks",
+            "localField": "task_id",
+            "foreignField": "id",
+            "as": "associatedTask"
+        }
+    },
+        doc! {
+        "$unwind": "$associatedTask"
+    },
+        doc! {
+        "$project": doc! {
+            "_id": 0,
+            "address": 1,
+            "task_id": 1,
+            "quest_id": "$associatedTask.quest_id"
+        }
+    },
+        doc! {
+        "$group": doc! {
+            "_id": "$quest_id",
+            "done": doc! {
+                "$sum": 1
+            }
+        }
+    },
+        doc! {
+        "$match": doc! {
+            "_id": quest_id,
+        }
+    },
+        doc! {
+        "$lookup": doc! {
+            "from": "tasks",
+            "localField": "_id",
+            "foreignField": "quest_id",
+            "as": "tasks"
+        }
+    },
+        doc! {
+        "$project": doc! {
+            "_id": 0,
+            "result": doc! {
+                "$cond": doc! {
+                    "if": doc! {
+                        "$eq": [
+                            doc! {
+                                "$size": "$tasks"
+                            },
+                            "$done"
+                        ]
+                    },
+                    "then": true,
+                    "else": false
+                }
+            }
+        }
+    },
+    ];
+    let tasks_collection = state.db.collection::<Document>("completed_tasks");
+    match tasks_collection.aggregate(pipeline, None).await {
+        Ok(cursor) => {
+            let mut cursor = cursor;
+            let mut result = false;
+            while let Some(doc) = cursor.try_next().await.unwrap() {
+                result = doc.get("result").unwrap().as_bool().unwrap();
+            }
+            (StatusCode::OK, Json(result)).into_response()
+        }
+        Err(_) => get_error("Error querying status".to_string()),
+    }
+}
