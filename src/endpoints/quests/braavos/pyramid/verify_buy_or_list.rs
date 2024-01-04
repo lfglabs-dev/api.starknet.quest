9b9d8442b82395eba70c9db2cce824432fec3659
ayushtom <tomar2585@gmail.com>
2024-01-04 18:09:07 +0530

    chore: update naming

--- src/endpoints/quests/braavos/pyramid/verify_buy_or_list.rs
+++ src/endpoints/quests/braavos/pyramid/verify_buy_or_list.rs
@@ -20,17 +20,16 @@ pub async fn handler(
 ) -> impl IntoResponse {
     let task_id = 107;
 
-    // make get request to focustree api for verification
+    // make get request to pyramid api for verification
     let url = format!(
         "{}/{}/isEligibleForQuest", state.conf.pyramid.api_endpoint,
         to_hex(query.addr)
     );
-    
+
     match fetch_json_from_url(url).await {
         Ok(response) => {
-            // check if user has sent NFT to focus tree wallet
-            let has_sent_nft = response.get("result").unwrap().as_bool().unwrap();
-            return if has_sent_nft {
+            let has_bought_or_listed_nft = response.get("result").unwrap().as_bool().unwrap();
+            return if has_bought_or_listed_nft {
                 match state.upsert_completed_task(query.addr, task_id).await {
                     Ok(_) => {
                         (StatusCode::OK, Json(json!({"res": true}))).into_response()
