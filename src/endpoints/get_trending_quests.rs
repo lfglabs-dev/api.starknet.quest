b8c3ada17f57f99665bfa9617d8a79774db3d12d
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-01-20 21:25:09 +0100

    feat: filter done quests in trending

--- src/endpoints/get_trending_quests.rs
+++ src/endpoints/get_trending_quests.rs
@@ -3,24 +3,32 @@ use crate::{
     utils::get_error,
 };
 use axum::{
-    extract::State,
-    http::StatusCode,
-    response::{IntoResponse, Json},
+    extract::{Query, State},
+    response::IntoResponse,
+    Json,
 };
 use futures::StreamExt;
-use mongodb::bson::doc;
-use mongodb::bson::from_document;
+use mongodb::bson::{doc, from_document};
+use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
+use starknet::core::types::FieldElement;
 use std::sync::Arc;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct NFTItem {
-    img: String,
-    level: u32,
+pub struct GetTrendingQuestsQuery {
+    addr: Option<FieldElement>,
 }
 
-pub async fn handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    let pipeline = vec![
+pub async fn handler(
+    State(state): State<Arc<AppState>>,
+    Query(query): Query<GetTrendingQuestsQuery>,
+) -> impl IntoResponse {
+    // Addr might not exist
+    let address = match query.addr {
+        Some(addr) => addr.to_string(),
+        None => "".to_string(),
+    };
+    let mut pipeline = vec![
         doc! {
             "$match": {
                 "disabled": false,
@@ -51,6 +59,65 @@ pub async fn handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
         },
     ];
 
+    // If address is provided, filter out quests that the user has already completed
+    if !address.is_empty() {
+        pipeline.extend_from_slice(&[
+            doc! {
+                "$lookup": {
+                        "from": "tasks",
+                        "localField": "id",
+                        "foreignField": "quest_id",
+                        "as": "tasks"
+                }
+            },
+            doc! {
+                "$lookup": doc! {
+                "from": "completed_tasks",
+                "let": {
+                    "task_ids": {
+                        "$map": {
+                            "input": "$tasks",
+                            "as": "taskObj",
+                            "in": "$$taskObj.id" // Extract the id from each object in the tasks array
+                        }
+                    }
+                },
+                "pipeline" : [
+                    {
+                        "$match": {
+                            "$expr": {
+                                "$and": [
+                                    {
+                                    "$in": ["$task_id", "$$task_ids"],
+                                    },
+                                    {
+                                    "$eq": ["$address", address],
+                                    }
+                                ]
+                            }
+                        }
+                    }
+                ],
+                "as": "completed_tasks"
+                }
+            },
+            doc! {
+                "$match": {
+                    "$expr": {
+                        "$ne": [
+                            {
+                                "$size": "$tasks",
+                            },
+                            {
+                                "$size": "$completed_tasks",
+                            },
+                        ],
+                    },
+                }
+            },
+        ]);
+    }
+
     let collection = state.db.collection::<QuestDocument>("quests");
     match collection.aggregate(pipeline, None).await {
         Ok(mut cursor) => {
@@ -67,6 +134,9 @@ pub async fn handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
             }
             (StatusCode::OK, Json(quests)).into_response()
         }
-        Err(_) => get_error("Error querying quests".to_string()),
+        Err(e) => {
+            println!("{:?}", e);
+            get_error("Error querying quests".to_string())
+        }
     }
 }
