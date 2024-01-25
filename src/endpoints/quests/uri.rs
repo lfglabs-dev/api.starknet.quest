01f9fe86d23ba216736912a9f5460abf2ca486fa
Iris <iris.devillars@gmail.com>
2024-01-25 15:42:44 +0100

    ref: update nimbora nft name and desc

--- src/endpoints/quests/uri.rs
+++ src/endpoints/quests/uri.rs
@@ -387,9 +387,9 @@ pub async fn handler(
         Some(35) => (
             StatusCode::OK,
             Json(TokenURI {
-                name: "Nimbora NFT name".into(),
-                description: "Nimbora NFT name".into(),
-                image: format!("{}/nimbora/nft.webp", state.conf.variables.app_link),
+                name: "The Nimbora Pool".into(),
+                description: "A Nimbora NFT won for successfully finishing the Quest. Nimbora is bridging Ethereum's Layer 1 and Layer 2 seamlessly for cost-efficient DeFi interactions with improved user experience and uncompromised pooling.".into(),
+                image: format!("{}/nimbora/pool.webp", state.conf.variables.app_link),
                 attributes: None,
             }),
         ).into_response(),
