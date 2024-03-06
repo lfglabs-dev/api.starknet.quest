8bb672b1f3f1082f200fa3042b8f54f5a97e762d
ayushtom <tomar2585@gmail.com>
2024-03-06 21:34:13 +0530

    feat: update param

--- src/endpoints/unique_page_visit.rs
+++ src/endpoints/unique_page_visit.rs
@@ -21,22 +21,21 @@ use crate::models::UniquePageVisit;
 
 #[derive(Deserialize)]
 pub struct GetQuestsQuery {
-    id: u32,
+    page_id: String,
 }
 
-#[route(post, "/unique_page_visit", crate::endpoints::unique_page_visit)]
+#[route(get, "/unique_page_visit", crate::endpoints::unique_page_visit)]
 pub async fn handler(
     State(state): State<Arc<AppState>>,
     Query(query): Query<GetQuestsQuery>,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
 ) -> impl IntoResponse {
-    let id = query.id;
-    let page_id = "quest_".to_owned() + id.to_string().as_str();
+    let id = query.page_id;
     let unique_viewers_collection: Collection<UniquePageVisit> =
         state.db.collection("unique_viewers");
     let created_at = Utc::now().timestamp_millis();
-    let filter = doc! { "viewer_ip": addr.to_string(), "viewed_page_id": &page_id };
-    let update = doc! { "$setOnInsert": { "viewer_ip": addr.to_string(), "viewed_page_id": &page_id,"timestamp":created_at } };
+    let filter = doc! { "viewer_ip": addr.to_string(), "viewed_page_id": &id };
+    let update = doc! { "$setOnInsert": { "viewer_ip": addr.to_string(), "viewed_page_id": &id,"timestamp":created_at } };
     let options = UpdateOptions::builder().upsert(true).build();
 
     match unique_viewers_collection.update_one(filter, update, options)
