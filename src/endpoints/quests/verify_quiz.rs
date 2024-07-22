8b440500106007805d64be8e74e59577fa7b6675
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-07-22 18:17:44 +0200

    fix: task id type for quiz

--- src/endpoints/quests/verify_quiz.rs
+++ src/endpoints/quests/verify_quiz.rs
@@ -33,7 +33,7 @@ pub async fn handler(
         Ok(mut cursor) => {
             let mut id = 0;
             while let Some(result) = cursor.try_next().await.unwrap() {
-                id = result.get("id").unwrap().as_i64().unwrap();
+                id = result.get("id").unwrap().as_i32().unwrap();
             }
             id as u32
         }
